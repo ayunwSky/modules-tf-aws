33764fa583dc4ffd17d4b76e586cbe7aa5148339
ayunwSky <28972707+ayunwSky@users.noreply.github.com>
2024-01-20 09:53:03 +0800

    feat: Add variables.tf

---
+++ vpc/variables.tf
@@ -0,0 +1,59 @@
+variable "create" {
+    default = true
+    type = bool
+    description = "Whether to create resources"
+}
+
+variable "cloud" {
+  type = string
+}
+
+variable "vpc_name" {
+  default = ""
+  type    = string
+}
+
+variable "vpc_cidr" {
+  default = ""
+  type    = string
+}
+
+variable "tags" {
+  default = {}
+  type    = map(string)
+}
+
+variable "subnet_name" {
+  default = ""
+  type    = string
+}
+
+variable "az_name" {
+  default = ""
+  type    = string
+}
+
+variable "subnet_cidr" {
+  default = ""
+  type    = string
+}
+
+variable "description" {
+  default = ""
+  type    = string
+}
+
+variable "vpc_id" {
+  default = ""
+  type    = string
+}
+
+variable "res_type" {
+  type = string
+}
+
+variable "auto_assign_public_ip" {
+  default     = false
+  type        = bool
+  description = "是否为该子网中的实例启用自动分配公共IP,一般只会为public subnet中的实例自动分配公共IP"
+}
